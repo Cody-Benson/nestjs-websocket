9b7bc1df48d98045e3a497d0f92b688eab397040
cody <codyobenson@gmail.com>
2025-06-11 17:15:10 -0500

    added chat gateway with minimal chat features

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,9 +1,27 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
+import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { Socket,Server } from "socket.io";
 
 @WebSocketGateway(80,{})
-export class ChatGateway{
-    @SubscribeMessage('newMessages')
-    handleNewMessage(@MessageBody() message:any){
-        console.log(message);
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+    @WebSocketServer() server:Server;
+
+    handleConnection(client: Socket, ...args: any[]) {
+        //broadcast to everyone but the client connecting
+        client.broadcast.emit('chat',`User ${client.id} has connected`);
+    }
+
+    handleDisconnect(client: Socket) {
+        //broadcast to everyone but the client disconnecting
+        client.broadcast.emit('chat',`User ${client.id} has connected`);
+    }
+    @SubscribeMessage('hello')
+    handleNewMessage(@ConnectedSocket() client:Socket, @MessageBody() message:any){
+        //this.server.emit('chat',message);
+        client.broadcast.emit('chat',message);
+    }
+
+    @SubscribeMessage('goodbye')
+    handleGoodByeMessage(@MessageBody() message:any){
+        this.server.emit('chat',message);
     }
 }
